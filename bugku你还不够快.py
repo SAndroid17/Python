7330b36a2de65d046794f72747822e5c4f534046
SAndroid17 <53066394+SAndroid17@users.noreply.github.com>
2020-02-21 21:20:26 +0800

    bugku你还不够快.py
    bugku你还不够快.

--- bugku你还不够快.py
+++ bugku你还不够快.py
@@ -7,7 +7,7 @@ res=req.get(url)#请求网页
 flag = res.headers['flag']#header里面搜索flag的报头信息
 txt1 = str(base64.b64decode(flag),encoding='utf-8')#用base64解码再将其给txt变量
 txt1 = txt1.split(" ",1)#切片并获取后半部分
-txt1= base64.b64decode(txt1[1])#base64解码成纯数字
+txt1= base64.b64decode(txt1[1])#base64解码成纯数字形式
 txt1=str(txt1,'utf-8')
 ans=req.post(url=url, data={'margin':txt1})
 print(ans.text)
